5c292446d0fb98e8ec544542d785cfeb866d4d0e
leonardobarone <leonardobarone26@gmail.com>
2021-10-01 16:36:36 +0200

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <img class="logo" src="../images/spotify-logo.png" alt="">
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    img: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  .logo {
+    width: 50px;
+  }
+</style>
