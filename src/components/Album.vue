7cbaaae9a128978ddd9f3f86c8e626826d200a5c
leonardobarone <leonardobarone26@gmail.com>
2021-10-02 12:32:49 +0200

    estrapolato card/album

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,16 +1,56 @@
-<template>
-  <div class="album">
-
-  </div>
+<template> 
+     
+        <div class="album">
+          <img class="poster" :src="album.poster" :alt="`${album.title} poster-img`">
+          <h2 class="title">{{album.title.toUpperCase()}}</h2>
+          <div class="author">{{album.author}}</div>
+          <div class="year">{{album.year}}</div>
+        </div>
+    
 </template>
 
 <script>
+
 export default {
   name: 'Album',
+  props: {
+    album: Object
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
+   
+
+  .album {
+    height: 100%;
+    text-align: center;
+    background-color: var(--bg-header);
+    margin: 2rem 1.5rem 0rem 1.5rem;
+  }
+
+  .poster {
+    padding: 1.25rem 0rem;
+    width: 80%;
+  }
+
+  .title {
+    color: white;
+    font-size: 1rem;
+  }
+
+  .author,
+  .year,
+  .title {
+    padding: 0px calc(20%);
+  }
+
+  .author,
+  .year {
+    color: gray;
+    font-size: .875rem;
+  }
 
 </style>
--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="albums container">
     <div class="row row-cols-2 row-cols-md-5">
-      <div class="col" v-for="(album, index) in albums" :key="index">
-        <div class="album">
-          <img class="poster" :src="album.poster" :alt="`${album.title} poster-img`">
-          <h2 class="title">{{album.title.toUpperCase()}}</h2>
-          <div class="author">{{album.author}}</div>
-          <div class="year">{{album.year}}</div>
-        </div>
-      </div>
+       <div class="col" v-for="(album, index) in albums" :key="index">
+          <Album :album="album" />
+       </div>
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import Album from './Album.vue'
 
 export default {
   name: 'Albums',
@@ -23,11 +19,13 @@ export default {
       albums:[]
       }
   },
+  components: {
+    Album
+  },
   created() {
     axios.get('https://flynn.boolean.careers/exercises/api/array/music')
     .then((response)=> {
         this.albums = response.data.response;
-        console.log(this.albums)
     });
   } 
 }
@@ -44,27 +42,9 @@ export default {
      padding-bottom: 2rem;
    }
 
-  .album {
-    text-align: center;
-    background-color: var(--bg-header);
-    padding: .3125rem .3125rem 3.1875rem .3125rem;
-    margin: 2rem 1.5rem 0rem 1.5rem;
-  }
+   .col {
+     margin-bottom: 20px;
+   }
 
-  .poster {
-    padding: 1.25rem 0rem;
-    width: 80%;
-  }
-
-  .title {
-    color: white;
-    font-size: 1rem;
-  }
-
-  .author,
-  .year {
-    color: gray;
-    font-size: .875rem;
-  }
 
 </style>
